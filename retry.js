ecaf46baccdb67c1a02a4df630f80e8b1c43b59a
xusd320 <xusd320@gmail.com>
2018-10-17 12:01:21 +0800

    add retry

---
+++ retry.js
@@ -0,0 +1,40 @@
+'use strict';
+
+class retry{
+ constructor() {}
+ 
+ static attempt(maxRetryTimes, func, ...args) {
+  var retryTimes = 0;
+  var resolved   = false;
+  var running    = false;
+  var ret;
+  
+  do {
+    if(!resolved && !running) {
+     retryTimes ++;
+     running = true;
+     
+     if(retryTimes > maxRetryTimes) {
+      console.log('Failed, maxRetryTimes reached !');
+      resolved = true;
+      return ret;
+     }
+     
+     console.log(`Retry time = ${retryTimes} , maxRetryTimes = ${maxRetryTimes}`);
+     
+     await func(args).then(result ={
+      running = false;
+      resolved = true;
+      ret = result;
+     }, err => {
+       running = false;
+       console.log('Failed, retry again !');
+     });
+    } else {
+      return ret;
+    }
+  } while(1);
+ }
+};
+
+module.exports = retry;
